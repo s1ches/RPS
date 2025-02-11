bf5aee75b22f9328f9439606d5c1cb0320398e98
Ivan Sosorin <ivansosorin@MacBook-Pro-Ivan.local>
2025-02-11 14:08:49 +0300

    remove dublicate code

--- backend/RPS.Common/Options/CorsOptions.cs
+++ backend/RPS.Common/Options/CorsOptions.cs
@@ -5,5 +5,4 @@ public class CorsOptions
     public const string CorsPolicyName = "AllowAll";
     
     public string[] AllowedOrigins { get; set; } = [];
-    public const string CorsPolicyName = "AllowAll";
 }
